95e3f0db74340c6098e57e7ad5d0d5e293b5a6d7
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-07-14 15:09:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,18 +19,31 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT name, age, SUM(amount)
   FROM users
   JOIN pledges
+<<<<<<< HEAD
   ON  users.id = pledges.user_id
+=======
+  ON  users.id = pledges.project_id
+>>>>>>> 955aeebc654814bd223ad9739521b9a8c7ef0e88
   GROUP BY users.id
   ORDER BY name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+<<<<<<< HEAD
  "SELECT title, SUM(amount) - funding_goal
   FROM projects
   JOIN pledges
   ON  projects.id = pledges.project_id
   GROUP BY projects.id 
   HAVING SUM(amount) >= funding_goal"
+=======
+ "SELECT title, funding_goal - SUM(amount)
+  FROM projects
+  JOIN pledges
+  ON  projects.id = pledges.user_id
+  WHERE funding_goal > SUM(amount)
+  GROUP BY projects.id"
+>>>>>>> 955aeebc654814bd223ad9739521b9a8c7ef0e88
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
